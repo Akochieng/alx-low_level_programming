b9e1fc46cdbb6de927b446abee334d44154e648a
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-16 23:22:36 +0000

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
-
 /**
   *main: program to test printf
   *return: 0  is success.
   */
-
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
