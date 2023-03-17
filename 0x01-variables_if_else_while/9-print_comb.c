ef4274483cdfe3bd064e5b367087319bdbecfb64
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-17 11:48:32 +0000

    print combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,13 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= '8'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(' ');
 		putchar(i);
-		putchar(',');
+		if (i < '9')
+			putchar(',');
 	}
-	putchar('9');
+	putchar('\n');
 	return (0);
 }
