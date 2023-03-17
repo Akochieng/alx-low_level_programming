2236efd3ec767931a074b4f6724e265f6268f50c
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-17 11:14:33 +0000

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main - prints the alphabet in lower case
+  *
+  *Return: 0 (success)
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'q' || i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
