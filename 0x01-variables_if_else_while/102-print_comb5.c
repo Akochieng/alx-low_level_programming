a909528feb0858133ea9ac5de741a5a302ce1183
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-17 17:23:52 +0000

    Print comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  *main - prints all possible combinations of two digits
+  *
+  *Return: 0 (Success)
+  */
+
+int main(void)
+{
+	int i, j, k, l;
+	int mini, minj;
+
+	for (i = '0'; i <= '9'; i++)
+		for (j = '0'; j <= '9'; j++)
+			for (k = i; k <= '9'; k++)
+				for (l = j; l <= '9'; l++)
+					if (!(i == j & j == k && k == l && l == i))
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+						putchar(',');
+					}
+	putchar('\n');
+	return (0);
+}
