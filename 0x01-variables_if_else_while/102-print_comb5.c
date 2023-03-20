d7adc8fe125c818430b17047ce67104c5cbf2b1b
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-20 19:34:28 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,21 +8,23 @@
 
 int main(void)
 {
-	int i, j, k, l;
+	int i, j;
 
-	for (i = '0'; i <= '9'; i++)
-		for (j = '0'; j <= '9'; j++)
-			for (k = i; k <= '9'; k++)
-				for (l = j + 1; l <= '9'; l++)
+	for (i = 0; i < 100; i++)
+		for (j = 0; j < 100; j++)
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
-					putchar(i);
-					putchar(j);
+					putchar(',');
 					putchar(' ');
-					putchar(k);
-					putchar(l);
-					if (!((i == 9) && (j == 8) && (k == 9) && (l == 9)))
-						putchar(',');
 				}
+			}
 	putchar('\n');
 	return (0);
 }
