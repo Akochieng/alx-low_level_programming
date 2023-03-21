83ce1b67d380e015a555cb99b36f1c72cb522194
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-21 17:21:36 +0000

    alphabet letters

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *main - check code
+  *
+  *Return: Always 0
+  *
+  *print_alphabet_x10 - prints the alphabet and a new line ten times
+  *
+  *Return: void
+  *
+  */
+void print_alphabet_x10(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet_x10(void)
+{
+	int c, i;
+
+	c = 'a';
+	i = 0;
+	while (i < 10)
+	{
+		while (c <= 'z')
+			putchar(c++);
+		putchar('\n');
+		i++;
+	}
+}
+
