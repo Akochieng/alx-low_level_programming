89e21542ced35f0e64d87498e2f1724c8bb23d7b
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-21 17:01:36 +0000

    0-putchar submission

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  *main -  prints _putchar followed by a new line
+  *
+  *Return: 0 (Success)
+  *
+  */
+
+int main(void)
+{
+	char word[] = "_putchar";
+	int c;
+	
+	c = 0;
+	while (word[c] != '\0')
+	{
+		putchar(word[c]);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
