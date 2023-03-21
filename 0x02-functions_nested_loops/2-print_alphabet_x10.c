cd8113b9a93417ecc0ee463cceefb5a557ed48a6
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-21 20:17:16 +0000

    is lower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
-  *
   *print_alphabet_x10 - prints the alphabet and a new line ten times
   *
   *Return: void
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *_islower - checks whether character is lowercase
+  *
+  *Return: 1 is lowercase, 0 otherwise
+  *
+  */
+int _islower(int c)
+{
+	int result;
+	
+	return result = c >= 'a' && c <= 'z' ? '1' : '0';
+}
