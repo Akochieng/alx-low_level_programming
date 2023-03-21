82815a6e8627751cf16d00b08bdbfe4eee057620
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-21 19:54:51 +0000

    add solution

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *add - returns the result of adding two intergers
+  *
+  *Return: sum of the intergers
+  *
+  */
+int add(int x, int y)
+{
+	return (x + y);
+}
