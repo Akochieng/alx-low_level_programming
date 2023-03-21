a50058fa4023eee33d3a4b35ebfe5b9521f28e9e
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-21 21:05:21 +0000

    10-add betty update

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,8 +3,8 @@
 
 /**
   *add - returns the result of adding two intergers
-  *@parameter x: input interger to be added
-  *@parameter y: input interger to be added
+  *@x: input interger to be added
+  *@y: input interger to be added
   *
   *Description: returns the sum of x and y as intergers.
   *Return: sum of the intergers
