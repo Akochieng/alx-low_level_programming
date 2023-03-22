e01b6c55023f2a70f66a636ba1881b6b76993962
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-22 16:40:24 +0000

    upload 100 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *print_times_table - prints the n times table starting with 0
+  *@n is the value of the number that the table will be based on
+  *
+  *Description: the function prints the n times table starting 
+  *with 0, if the number is greater than 15 or less than 0,
+  *then nothing is printed
+  *
+  *Return: void
+  */
+
+void print_times_table(int n)
+{
+	int a, b;
+
+	a = 0;
+	if ((n > 0) && (n < 15))
+		while (a <= n)
+		{
+			b = 0;
+			while (b <= n)
+			{	
+				if (b != n)
+					printf("%3d ", a * b);
+				else
+					printf("%3d\n", a * b);
+				b++;
+			}
+			a++;
+		}
+
+}
