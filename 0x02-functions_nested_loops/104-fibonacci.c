a6f5c1bb039f23416f6231df0dada3fc1b510c7e
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-22 21:32:31 +0000

    update 104 fibonnacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+  *main - prints the first 98 fibonacci numbers
+  *
+  *Description: prints the first 98 fibonacci numbers followed
+  *by a new line
+  *Return: 0 (success)
+  */
+
+
+
+
+int main(void)
+{
+	long double cur, prev, temp;
+	int counter;
+
+	prev = temp = 0;
+	counter = 0;
+	cur = 1;
+	while (counter < 98)
+	{
+		temp = cur;
+		cur = cur + prev;
+		prev = temp;
+		counter++;
+		if (counter == 98)
+			printf("%.0Lf\n", cur);
+		else if (counter < 98)
+			printf("%.0Lf, ", cur);
+	}
+	return (0);
+}
