f60a42c34355011e145f98c5031f02f921ac6b1b
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-23 18:53:37 +0000

    update 5 more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  *more_numbers - prints 10 times the numbers from 0 to 14
+  *
+  *Description: prints 10 times the numbers from 0 to 14
+  *using a max of 3 putchar
+  *Return void
+  */
+
+void more_numbers(void)
+{
+	int i, t;
+
+	i = 0;
+	while (i++ <= 10)
+	{
+		for (t = 0; t <= 14; t++)
+		{
+			if (t > 9)
+				putchar('0' + (t / 10));
+			putchar('0' + (t % 10));
+		}
+		putchar('\n');
+	}
+
+}
