c08ce97a66abaa1a444ccc102e0651e832c9a9c3
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-23 21:51:52 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,14 @@ int main(void)
 
 	for (i = 0; i <= 100; i++)
 	{
-		if (!((i % 3 == 0) || (i % 5 == 0)))
-			printf("%d", i);
+		if ((i % 3 == 0) && (i % 5 == 0) && i != 0)
+			printf("FizzBuzz");
+		else if ((i % 3 == 0) && (i != 0))
+			printf("Fizz");
+		else if ((i % 5 == 0) && (i != 0))
+			printf("Buzz");
 		else
-		{
-			if (i % 3 == 0)
-				printf("Fizz");
-			if (i % 5 == 0)
-				printf("Buzz");
-		}
+			printf("%d", i);
 		putchar(' ');
 	}
 	putchar('\n');
