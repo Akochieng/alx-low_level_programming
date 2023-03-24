b55b50f4696fc0844670f0c13cec35378d2323e6
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-24 12:13:38 +0000

    correct print diagonal c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,17 +9,25 @@
   */
 void print_diagonal(int n)
 {
-	int a, i;
-
-	for (i = 0; i <= n; i++)
+	if (n <= 0)
 	{
-		a = i - 1;
-		while (a > 0)
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+
+		for (i = 0; i < n; i++)
 		{
-			putchar(' ');
-			a--;
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
-		putchar('\\');
-		putchar('\n');
 	}
 }
