512e879de0624843994a98eebe5af4bc684d0fd4
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-27 19:11:12 +0000

    upload print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *print_array - prints n elements of an array a
+  *@a: the array
+  *@n: the number of elements
+  *Description: prints the elements of an array
+  *Return: void
+  */
+void print_array(int *a, int n)
+{
+	for (; n > 0; --n)
+	{
+		printf("%d", *a);
+		if (n == 1)
+			putchar('\n');
+		else
+			printf(", ");
+		a++;
+	}
+}
