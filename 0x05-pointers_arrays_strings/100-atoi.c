91bdadd0d6011f26508300177e88da4827bf0a27
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-27 21:16:41 +0000

    add atoi function

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  *_atoi - converts a string to interger
+  *@s: the input string
+  *Description: converts a string to intergers while ignoring any
+  *non intergers.
+  *Return: the interger
+  */
+int _atoi(char *s)
+{
+	int result;
+	int sign;
+
+	sign = 1;
+	result = 0;
+	while (*s != '\0')
+	{
+		if (*s == '-' || *s == '+')
+			sign = *s == '-' ? sign * -1 : sign;
+		if (*s >= '0' && *s <= '9')
+			result = (result * 10) + (*s - '0');
+		s++;
+	}
+	return (result * sign);
+
+}
