a60e7a8b6d8979aefdf8ea2992602195fb757af2
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-28 19:33:05 +0000

    update 101 keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,21 +6,21 @@
   *
   *Return: 0 success
   */
-int main()
+int main(void)
 {
 	int len, i, temp;
 	char pass[16];
 
+	srand(time(NULL));
 	len = (time(NULL) % 8) + 8;
 	for (i = 0; i < len; )
 	{
 		temp = rand() % 123;
-		if ((temp >= 48 && temp <= 57) || (temp >= 65 && temp <= 90) \
-			|| (temp >= 97 && temp <= 122))
-		{
+		if ((temp >= 65 && temp <= 90) || (temp >= 97 && temp <= 122))
 			pass[i] = (char)temp;
-			i++;
-		}
+		else
+			pass[i] = '0' + (temp % 10);
+		i++;
 	}
 
 	pass[i] = '\0';
