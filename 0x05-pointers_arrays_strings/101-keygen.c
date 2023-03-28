61a58be616d0d6bb038fc04d2755c117f690fa75
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-28 22:13:02 +0000

    update reverse string

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define MAXPASSLEN 16
 /**
   *main - generates random valid passwords
   *
@@ -9,19 +10,18 @@
 int main(void)
 {
 	int len, i, temp;
-	char pass[16];
+	char pass[MAXPASSLEN];
 
 	srand(time(NULL));
 	len = (time(NULL) % 8) + 8;
-	for (i = 0; i < len; )
-	{
+	do{
 		temp = rand() % 123;
 		if ((temp >= 65 && temp <= 90) || (temp >= 97 && temp <= 122))
 			pass[i] = (char)temp;
 		else
 			pass[i] = '0' + (temp % 10);
 		i++;
-	}
+	} while (i < len);
 
 	pass[i] = '\0';
 	for (i = 0; pass[i] != '\0'; i++)
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,6 +8,17 @@
   */
 void rev_string(char *s)
 {
-	
+	int len, i;
+	char temp;
 
+	len = 0;
+	while (s[len])
+		len++;
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
 }
+
