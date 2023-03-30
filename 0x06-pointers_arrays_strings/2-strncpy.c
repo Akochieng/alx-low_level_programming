a2790e41268c3e203e837d209f4bdd866f42275d
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-30 10:59:16 +0000

    update strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,8 @@
   */
 char *_strncpy(char *dest, char *src, int n)
 {
-	while ((*dest == *src) && n--)
-		;
-	return (dest - n);
+	char *temp = dest;
+
+	while ((*temp++ = *src++) && n--);
+	return (dest);
 }
