718ad7c0a0b817a46ce98cb480b14d3332e697d0
Alphonce Kochieng <dulkochieng@gmail.com>
2023-03-31 16:10:49 +0000

    correct strcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,8 @@
   *@dest: the string that is appended to
   *@src: the string that is copied from
   *@n: the number of characters to append
-  *Description: appends n characters from src to dest, overwriting the terminating
+  *Description: appends n characters from src to dest, overwriting the
+  *terminating
   *null byte at the end of dest
   *Return: dest(the resulting string)
   */
@@ -12,12 +13,13 @@ char *_strncat(char *dest, char *src, int n)
 {
 	char *temp;
 
-	temp = dest;	
+	temp = dest;
 	for ( ; *temp++ != '\0'; )
 		;
 	temp = temp - 1;
-	while ((*temp++ = *src++) != '\0' && --n)
-		;
+	if (n > 0)
+		while ((*temp++ = *src++) != '\0' && --n)
+			;
 	*temp = '\0';
 	return (dest);
 }
