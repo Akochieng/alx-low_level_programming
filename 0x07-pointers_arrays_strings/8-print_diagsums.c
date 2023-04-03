78b058abd88a3c8df2c9399b213f3613bfe96271
Alphonce Kochieng <dulkochieng@gmail.com>
2023-04-03 21:42:46 +0000

    update print chessboard

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *print_diagsums - prints the sum of the two diagonals of a square matrix
+  *@a: the matrix
+  *@size: the size of the matrix
+  *@Description: prints the sum of the two diagonals of a square matrix of
+  *size size.
+  *Return: null
+  */
+void print_diagsums(int *a, int size)
+{
+	int sum;
+
+	while 
+}
