c96ee5a28c04ab904f6fd155fd4224d4ed5965df
Alphonce Kochieng <dulkochieng@gmail.com>
2023-04-04 16:05:26 +0000

    print the sum of diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,12 +3,12 @@
   *print_diagsums - prints the sum of the two diagonals of a square matrix
   *@a: the matrix
   *@size: the size of the matrix
-  *@Description: prints the sum of the two diagonals of a square matrix of
+  *Description: prints the sum of the two diagonals of a square matrix of
   *size size.
   *Return: null
   */
 void print_diagsums(int *a, int size)
-{	
+{
 	int *ptr;
 	int i, j, sumpos, sumneg;
 
@@ -19,7 +19,7 @@ void print_diagsums(int *a, int size)
 		{
 			if ((i + j) == (size - 1))
 				sumneg = sumneg + *((ptr + j) + (i * size));
-			if ( i == j)
+			if (i == j)
 				sumpos = sumpos + *((ptr + j) + (i * size));
 		}
 	printf("%d, %d\n", sumpos, sumneg);
