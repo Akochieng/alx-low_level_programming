960970202cff2af8fe70b70d11dda5941f04716b
Alphonce Kochieng <dulkochieng@gmail.com>
2023-04-08 20:27:24 +0000

    update coin calculator function

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - prints the minimum number of coins to make change
+  *@argc: the number of arguments in the program
+  *@argv: the arguments accepted by the program
+  *Description: computes the minimum number of coins needed to make change,
+  *only accepting one argument, and printing error if more or less args are
+  *added.
+  *Return: 0 if successful or 1 if error
+  */
+int main(int argc, char **argv)
+{
+	int amount, coins, counter, state;
+	int values[5] = {25, 10, 5, 2, 1};
+
+	coins = state = 0;
+	if (argc == 2)
+	{
+		amount = atoi(*++argv);
+		for (counter = 0; counter < 5; counter++)
+		{
+			coins += amount / *(values + counter);
+			amount = amount % *(values + counter);
+		}
+	}
+	else
+		state = 1;
+	state == 0 ? printf("%d\n", coins) : printf("%s\n", "Error");
+	return (state);
+}
