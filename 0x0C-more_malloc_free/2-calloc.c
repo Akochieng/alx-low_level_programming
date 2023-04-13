db21428205503a1b38210374c6f280964a040249
Alphonce Kochieng <dulkochieng@gmail.com>
2023-04-13 13:45:24 +0000

    update calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+  *_calloc - allocates memory of nmemb elements of size bytes
+  *@nmemb: number of elements
+  *@size: size of each of the elements
+  *Description: allocates memory of nmemb elements of size bytes and sets
+  *them to 0
+  *Return: 0 if successfull and NULL if failed.
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int num = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (num = 0; num < size; num++)
+		*(p + num) = 0;
+	return (p);
+}
