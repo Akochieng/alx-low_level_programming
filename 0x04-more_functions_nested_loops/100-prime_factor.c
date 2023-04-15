a332e212bbb6b425d1d8a81e5e2d0a6ea7bfb939
Alphonce Kochieng <dulkochieng@gmail.com>
2023-04-15 16:13:58 +0000

    correct prime factor function

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,2 +1,42 @@
 #include <stdio.h>
+#include <math.h>
+/**
+  *main - print the largest prime number that can divide 612852475143
+  *
+  *Description: prints the largest prime number that can divide 612852475143
+  *and prints it with a helper function primecalc.
+  *Return: (0) for success
+  */
+int main(void)
+{
+	long int n, primenum;
 
+	n = 612852475143;
+	while (primenum < sqrt(n))
+	{
+		primenum = primecalc(n);
+		while (n % primenum == 0 && n != primenum)
+			n = n / primenum;
+	}
+	printf("%ld\n", primenum);
+	return (0);
+
+}
+/**
+  *primecalc - looks for the largest prime number that can divide a number n
+  *@n: the number whose prime number is to be computed
+  *Description: as a helper function, it increments a number num until the
+  *largest prime number that can divide n is reached. If num becomes equal
+  *or larger than the sqrt of n, then num is taken to be equal to n
+  *Return: long int version of the largest prime number that can divide n
+  */
+
+long int primecalc(long int n)
+{
+	long int num = 2;
+
+	while (n % num != 0 && num++ < sqrt(n))
+		;
+	num = num < sqrt(n) ? num : n;
+	return (num);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,3 +11,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 int _putchar(char c);
+long int primecalc(long int n);
