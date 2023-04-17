bfb9d5256539518dcde30f773842fd6e4e80f52f
Alphonce Kochieng <dulkochieng@gmail.com>
2023-04-17 13:51:21 +0000

    upload struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+  *struct dog - consists of elements name, age and owner
+  *@name: name of the dog
+  *@age: age of the dog
+  *@owner: owner of the dog
+  *Description: struct describing a dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
