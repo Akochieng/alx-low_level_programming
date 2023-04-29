70ef9d7896276ffa33fbd965cf108a3b329459e7
Alphonce Kochieng <dulkochieng@gmail.com>
2023-04-29 11:31:35 +0000

    print list function

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+  *print_listint - prints all the elements of a list
+  *@h: the head of the list
+  *
+  *Return: the number of nodes
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t a = 0;
+	const listint_t *cur;
+
+	cur = h;
+	while (cur)
+	{
+		print_int(cur->n);
+		cur = cur->next;
+		a++;
+		_putchar('\n');
+	}
+	return (a);
+}
+/**
+  *print_int - prints an interger to the stdout
+  *@num: the number to be printed
+  *
+  *Return: number of characters printed
+  */
+int print_int(int num)
+{
+	if (num < 10)
+		return (_putchar(num + '0'));
+	return (print_int((num / 10) + _putchar((num % 10) + '0')));
+}
+/**
+  *_putchar - prints a character to the stdout
+  *@c: the character to be printed
+  *
+  *Return: number of characters printed
+  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
