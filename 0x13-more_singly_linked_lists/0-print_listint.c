346b4de3af8d1381bd3cfe29b161ef5589e03b15
Alphonce Kochieng <dulkochieng@gmail.com>
2023-05-01 18:04:09 +0000

    update printlist function

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -16,22 +16,9 @@ size_t print_listint(const listint_t *h)
 	cur = h;
 	while (cur)
 	{
-		print_int(cur->n);
+		printf("%d\n", cur->n);
 		cur = cur->next;
 		a++;
-		_putchar('\n');
 	}
 	return (a);
-}
-/**
-  *print_int - prints an interger to the stdout
-  *@num: the number to be printed
-  *
-  *Return: number of characters printed
-  */
-size_t print_int(int num)
-{
-	if (num < 10)
-		return (_putchar(num + '0'));
-	return (print_int(num / 10) + _putchar((num % 10) + '0'));
 }
