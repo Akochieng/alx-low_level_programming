31ff9e452afd721648667e96a39e179dfd66296b
Alphonce Kochieng <dulkochieng@gmail.com>
2023-05-09 11:16:05 +0000

    update cp function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,13 +20,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	if (!(buffer))
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	num = read(fd, buffer, letters);
-	if (!(write(1, buffer, num) == num))
+	if (!(write(STDOUT_FILENO, buffer, num) == num))
 		return (0);
 	free(buffer);
 	close(fd);
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,6 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #define BUFFSIZE 1024
-#define mode 0664
 /**
   *main - function to copy content of one file to another
   *@argc: the number of parameters passed in
@@ -16,35 +15,36 @@ int main(int argc, char **argv)
 	int fd_from, fd_to, res;
 	char *buffer;
 
+	res = 0;
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	buffer = malloc(BUFFSIZE);
 	if (buffer == NULL)
-		dprintf(2, "Error: Insufficient memory\n");
+		dprintf(STDERR_FILENO, "Error: Insufficient memory\n");
 	while ((res = read(fd_from, buffer, BUFFSIZE)) > 0)
-		if (write(fd_to, buffer, BUFFSIZE) != res)
+		if (write(fd_to, buffer, res) != res)
 		{
-			dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			exit(99);
 		}
 	if (close(fd_from) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 	if (close(fd_to) == -1)
-		dprintf(1, "Error: Can'it close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can'it close fd %d\n", fd_to);
 	return (0);
 }
