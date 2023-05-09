4a298a842a9d6a9300a729c9643834ba4e68e8a9
Alphonce Kochieng <dulkochieng@gmail.com>
2023-05-09 10:11:50 +0000

    correct the creat file function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,11 +16,11 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	size_t len;
 
-	if (filename == NULL)
+	if (!(filename))
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
 	len = _strlen(text_content);
 	if (len > 0)
 		if (write(fd, text_content, len) == -1)
