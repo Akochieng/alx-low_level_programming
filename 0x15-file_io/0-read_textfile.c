4f9b7422996f7810935ed6582cf24be982e619d0
Alphonce Kochieng <dulkochieng@gmail.com>
2023-05-09 10:37:42 +0000

    correct read file function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,14 +19,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	num = 0;
 	if (filename == NULL)
 		return (0);
-	buffer = malloc(BUFSIZ);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	read(fd, buffer, letters);
-	num += write(1, buffer, letters);
+	num = read(fd, buffer, letters);
+	if (!(write(1, buffer, num) == num))
+		return (0);
 	free(buffer);
 	close(fd);
 	return (num);
