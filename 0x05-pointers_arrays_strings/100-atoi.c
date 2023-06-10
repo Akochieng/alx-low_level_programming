6a04f2510992704b933be68f682464c221427b55
Alphonce Kochieng <dulkochieng@gmail.com>
2023-06-10 14:34:31 +0000

    corrections

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,15 +10,24 @@ int _atoi(char *s)
 {
 	int result;
 	int sign;
+	int state = 0;
 
 	sign = 1;
 	result = 0;
 	while (*s != '\0')
 	{
-		if (*s == '-' || *s == '+')
-			sign = *s == '-' ? sign * -1 : sign;
 		if (*s >= '0' && *s <= '9')
+		{
 			result = (result * 10) + (*s - '0');
+			state = 1;
+		}
+		else
+		{
+			if (*s == '-' || *s == '+')
+				sign = *s == '-' ? sign * -1 : sign;
+			else if (state == 1)
+				break;
+		}
 		s++;
 	}
 	return (result * sign);
