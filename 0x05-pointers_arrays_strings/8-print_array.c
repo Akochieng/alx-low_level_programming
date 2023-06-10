4f1db596daebbc24b35ade7d40c3baf7d9a44284
Alphonce Kochieng <dulkochieng@gmail.com>
2023-06-10 14:09:08 +0000

    update the correction

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,8 @@ void print_array(int *a __attribute__((unused)), int n)
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", a[i]);
-			if (i == n - 1)
-				putchar('\n');
-			else
+			if (!(i == n - 1))
 				printf(", ");
 		}
+	putchar('\n');
 }
