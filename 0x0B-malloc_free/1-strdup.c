e241ac24d7f0e869bccddde8cf0f98c6cb0e1335
Alphonce Kochieng <dulkochieng@gmail.com>
2023-06-13 19:31:23 +0000

    my strdup version

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+  *_strdup - creates memory and copies str contents to it
+  *@str: the string
+  *Description: creates memory using malloc and copies contents of str
+  *Return: NULL on failure, or pointer to copy of str
+  */
+char *_strdup(char *str)
+{
+	size_t len, i;
+	char *temp = NULL;
+
+	len = _strlen(str);
+	temp = malloc(len + 1);
+	if (temp == NULL)
+		temp = NULL;
+	if (temp)
+	{
+		for (i = 0; i < len; i++)
+			temp[i] = str[i];
+		temp[i] = '\0';
+	}
+	return (temp);
+}
+/**
+  *_strlen - find the length of a string
+  *@s: the string
+  *
+  *Return: the length of the string
+  */
+size_t _strlen(char *s)
+{
+	size_t len = 0;
+
+	for (len = 0; *s; s++, len++)
+		;
+	return (len);
+}
