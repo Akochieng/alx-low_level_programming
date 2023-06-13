e7695699ee88a5fa98ee9cab4297b412a3fa7f31
Alphonce Kochieng <dulkochieng@gmail.com>
2023-06-13 19:43:57 +0000

    free resources if alloc_grid fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,12 +16,19 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
+	}
 	for (h = 0; h < height; h++)
 	{
 		temp = malloc(sizeof(int) * width);
 		if (temp == NULL)
+		{
+			while (--h)
+				free(grid[h]);
 			return (NULL);
+		}
 		for (w = 0; w < width; w++)
 			temp[w] = 0;
 		grid[h] = temp;
