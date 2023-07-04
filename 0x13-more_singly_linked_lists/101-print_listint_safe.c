29adfd5d3129c32b9cbdbd923694e1ac5fec6c69
Alphonce Kochieng <dulkochieng@gmail.com>
2023-07-04 19:31:26 +0000

    print list function

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,24 +1,33 @@
 #include "lists.h"
-
+/**
+  *print_listint_safe - prints a linked list with loops
+  *@head: the head of the list
+  *
+  *Return: the number of nodes in the list
+  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *temp;
-	const listint_t *safety;
+	const listint_t *temp = NULL;
+	const listint_t *safety = NULL;
 	size_t num = 0;
 
 	if (head == NULL)
 		return (num);
 	temp = head;
-	if (temp->next->next)
-		safety = temp->next->next;
-	for(num = 0; temp != NULL; num++)
+	safety = temp->next;
+	for(num = 0; temp != NULL && temp != safety; num++)
 	{
-		if (temp == safety)
-			exit(98);
+		if (safety == temp)
+		{
+			printf("->[%p] %d\n", (void *)temp, temp->n);
+			break;
+		}
 		printf("[%p] %d\n", (void *)temp, temp->n);
 		temp = temp->next;
-		if (safety->next != NULL)
+		if (safety != NULL && safety->next != NULL)
 			safety = safety->next->next;
+		else if (safety != NULL)
+			safety = safety->next;
 	}
 	return (num);
 }
