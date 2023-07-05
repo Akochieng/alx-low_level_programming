1d8856806a3372d8a2388f1d61128ccccece172b
Alphonce Kochieng <dulkochieng@gmail.com>
2023-07-05 17:59:36 +0000

    correct free list function

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,6 +10,8 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 	listint_t *next;
 
+	if (h == NULL)
+		return;
 	if (!(*head == NULL))
 	{
 		temp = *head;
@@ -20,7 +22,5 @@ void free_listint2(listint_t **head)
 			temp = next;
 		}
 	}
-	else
-		return;
 	*head = NULL;
 }
